644281d7bd94ebf042e536a6cf335055d0334b42
Bonnie3206 <molk3206@gmail.com>
2021-04-07 10:51:45 +0800

    Initial Commit

---
+++ iOS_hw3_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_hw3_1
+//
+//  Created by CK on 2021/4/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_hw3_1/iOS_hw3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_hw3_1App.swift
+//  iOS_hw3_1
+//
+//  Created by CK on 2021/4/7.
+//
+
+import SwiftUI
+
+@main
+struct iOS_hw3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
